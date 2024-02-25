769431166c1aeee9b0a5a0d0850767cf302c42c7
Lee-Margo <eaten1216@gmail.com>
2024-02-25 16:02:04 +0800

    try2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/to-do-list-tailwind',
+  base: '/to-do-list-tailwind/',
   plugins: [
     vue()
   ],
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-export default defineConfig({
-    base: '/to-do-list-tailwind',
-    plugins: [
-      vue()
-    ]
-  })
